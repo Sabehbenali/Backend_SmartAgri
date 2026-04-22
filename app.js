421086688f5c6769459126ad8c8d12d4c01780ef
Sabehbenali <sabehbenali287@gmail.com>
2026-04-22 14:12:57 +0100

    Update.app.js

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const { connectToMongoDB } = require ('./config/mongo.connections');
+const { connectToMongoDB } = require('./config/mongo.connections');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users.routes');
+var usersRouter = require('./routes/user.routes');
 
-const http=require('http');
+// Nouvelles routes (avec les noms de fichiers renommés)
+var farmRouter = require('./routes/farm.routes');
+var sensorRouter = require('./routes/sensor.routes');
+var actuatorRouter = require('./routes/actuator.routes');
+var energyRouter = require('./routes/energy.routes');
+var eventRouter = require('./routes/event.routes');
 
+const http = require('http');
 require('dotenv').config();
 
 var app = express();
@@ -23,26 +29,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// Monter les nouvelles routes API sous le préfixe /api
+app.use('/api/farm', farmRouter);
+app.use('/api/sensors', sensorRouter);
+app.use('/api/actuators', actuatorRouter);
+app.use('/api/energy', energyRouter);
+app.use('/api/events', eventRouter);
+
 // catch 404 and forward to error handler
-//k yebda path ne existe pas
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-//ki yti7 el backend 
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.json('error');
 });
- 
-const server =http.createServer(app);
-server.listen(process.env.PORT,() => {
-  connectToMongoDB(),
+
+const server = http.createServer(app);
+server.listen(process.env.PORT, () => {
+  connectToMongoDB();
   console.log(`server is running on port ${process.env.PORT}`);
 });
