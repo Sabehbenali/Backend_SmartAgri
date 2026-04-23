91d60aebbde0bf46faaeb6d329219b8ae8003f96
Sabehbenali <sabehbenali287@gmail.com>
2026-04-23 01:42:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');  // ← AJOUT
 const { connectToMongoDB } = require('./config/mongo.connections');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/user.routes');
 
-// Nouvelles routes (avec les noms de fichiers renommés)
 var farmRouter = require('./routes/farm.routes');
 var sensorRouter = require('./routes/sensor.routes');
 var actuatorRouter = require('./routes/actuator.routes');
@@ -20,6 +20,8 @@ require('dotenv').config();
 
 var app = express();
 
+app.use(cors());  // ← AJOUT : active CORS
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -28,20 +30,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-// Monter les nouvelles routes API sous le préfixe /api
 app.use('/api/farm', farmRouter);
 app.use('/api/sensors', sensorRouter);
 app.use('/api/actuators', actuatorRouter);
 app.use('/api/energy', energyRouter);
 app.use('/api/events', eventRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -49,7 +47,7 @@ app.use(function(err, req, res, next) {
   res.json('error');
 });
 
-const server = http.createServer(app);//sna3na server
+const server = http.createServer(app);
 server.listen(process.env.PORT, () => {
   connectToMongoDB();
   console.log(`server is running on port ${process.env.PORT}`);
