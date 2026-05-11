77786d575bf29ac050c356bb7fd44cbad4088611
Sabehbenali <sabehbenali287@gmail.com>
2026-05-11 11:30:45 +0100

    filedeleted

--- models/farm.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const farmSchema = new mongoose.Schema({
-    nom: { type: String, default: '' },          // plus obligatoire
-    localisation: { type: String, default: '' }, // optionnel
-    utilisateurId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, unique: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Farm', farmSchema);
