3134ab01de2cacf40395ff99d2d61ac0603567cb
Sabehbenali <sabehbenali287@gmail.com>
2026-05-11 12:07:53 +0100

    Update utilisateur.model.js

--- models/utilisateur.model.js
+++ models/utilisateur.model.js
@@ -8,15 +8,18 @@ const utilisateurSchema = new mongoose.Schema({
     nom: String,
     telephone: String,
     dateInscription: { type: Date, default: Date.now },
-    profileImage: { type: String, default: '' }
+    profileImage: { type: String, default: '' },
+    resetPasswordToken: String,      // ← AJOUTÉ pour forgot password
+    resetPasswordExpires: Date        // ← AJOUTÉ pour forgot password
 }, { timestamps: true });
 
+// Middleware pour hasher le mot de passe automatiquement
 utilisateurSchema.pre('save', async function (next) {
     if (!this.isModified('motDePasseHash')) return next();
     try {
         const salt = await bcrypt.genSalt(10);
         this.motDePasseHash = await bcrypt.hash(this.motDePasseHash, salt);
-       
+        next();
     } catch (error) {
         next(error);
     }
